5c489dc99afe860d07d01a0f07035cca2ee0c567
dev-rome <haynesjerome@gmail.com>
2023-10-08 16:28:15 -0400

    add logic to show dropdown content and to change font family, and edit icon and switch background

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 // Get references to elements
 const body = document.body;
+const fontText = document.querySelector(".dropdown-font");
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownValue = document.querySelectorAll("a");
+const btnArrow = document.querySelector(".btn-arrow");
 const themeToggle = document.querySelector(".theme-toggle");
+const toggleSwitch = document.querySelector(".toggle-switch");
+const moonIcon = document.querySelector(".svg-moon");
 const circle = document.querySelector(".circle");
+
+btnArrow.addEventListener("click", function () {
+  dropdownContent.classList.toggle("hide");
+});
+
+// Select dropdown value, change font based on value and hide dropdown box
+dropdownValue.forEach((value) => {
+  value.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const fontType = value.textContent;
+
+    if (fontType === "Sans Serif") {
+      fontText.textContent = "Sans Serif";
+      body.style.fontFamily = "Inter, sans serif";
+      dropdownContent.classList.add("hide");
+    } else if (fontType === "Serif") {
+      fontText.textContent = "Serif";
+      body.style.fontFamily = "Lora, serif";
+    } else if (fontType === "Mono") {
+      fontText.textContent = "Mono";
+      body.style.fontFamily = "Inconsolata, monospace";
+    }
+
+    dropdownContent.classList.add("hide");
+  });
+});
 
 // Function to toggle between light and dark mode
 function toggleTheme() {
@@ -19,9 +52,13 @@ function updateThemeIcon() {
   if (body.classList.contains("dark-mode")) {
     circle.style.transform = "translate(50%, -50%)";
     circle.style.left = "calc(100% - 24px)";
+    toggleSwitch.style.backgroundColor = "#a445ed";
+    moonIcon.style.stroke = "#a445ed";
   } else {
     circle.style.transform = "translate(-50%, -50%)";
     circle.style.left = "10px";
+    toggleSwitch.style.backgroundColor = "#757575";
+    moonIcon.style.stroke = "#757575";
   }
 }
 
