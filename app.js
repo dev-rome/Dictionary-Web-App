b13c3af142b0d9ce8dbb0bb8fdd0c79909968ac9
dev-rome <haynesjerome@gmail.com>
2023-10-09 00:41:40 -0400

    add api to fetch dictionary word data, edit font family for input, and edit functions

--- app.js
+++ app.js
@@ -8,36 +8,28 @@ const themeToggle = document.querySelector(".theme-toggle");
 const toggleSwitch = document.querySelector(".toggle-switch");
 const moonIcon = document.querySelector(".svg-moon");
 const circle = document.querySelector(".circle");
+const form = document.querySelector("form");
+const inputText = document.querySelector("form input");
+const word = document.querySelector(".word");
+const phonetic = document.querySelector(".phonetic");
+const playButton = document.querySelector("#play-button")
 
-btnArrow.addEventListener("click", function () {
-  dropdownContent.classList.toggle("hide");
-});
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
-// Select dropdown value, change font based on value and hide dropdown box
-dropdownValue.forEach((value) => {
-  value.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const fontType = value.textContent;
-
-    if (fontType === "Sans Serif") {
-      fontText.textContent = "Sans Serif";
-      body.style.fontFamily = "Inter, sans serif";
-      dropdownContent.classList.add("hide");
-    } else if (fontType === "Serif") {
-      fontText.textContent = "Serif";
-      body.style.fontFamily = "Lora, serif";
-    } else if (fontType === "Mono") {
-      fontText.textContent = "Mono";
-      body.style.fontFamily = "Inconsolata, monospace";
-    }
-
-    dropdownContent.classList.add("hide");
-  });
-});
+// fetch api data
+const fetchData = async (inputValue) => {
+  try {
+    const res = await fetch(url + inputValue);
+    const data = await res.json();
+    word.textContent = data[0].word;
+    phonetic.textContent = data[0].phonetic
+  } catch (error) {
+    throw new Error("Failed to fetch data.");
+  }
+};
 
 // Function to toggle between light and dark mode
-function toggleTheme() {
+const toggleTheme = () => {
   if (body.classList.contains("dark-mode")) {
     // If dark mode is active, switch to light mode
     body.classList.remove("dark-mode");
@@ -45,10 +37,10 @@ function toggleTheme() {
     // If light mode is active, switch to dark mode
     body.classList.add("dark-mode");
   }
-}
+};
 
 // Function to update the theme toggle icon based on the current theme
-function updateThemeIcon() {
+const updateThemeIcon = () => {
   if (body.classList.contains("dark-mode")) {
     circle.style.transform = "translate(50%, -50%)";
     circle.style.left = "calc(100% - 24px)";
@@ -60,13 +52,53 @@ function updateThemeIcon() {
     toggleSwitch.style.backgroundColor = "#757575";
     moonIcon.style.stroke = "#757575";
   }
-}
+};
+
+// Select dropdown value, change font based on value and hide dropdown box
+dropdownValue.forEach((value) => {
+  value.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const fontType = value.textContent;
+
+    if (fontType === "Sans Serif") {
+      fontText.textContent = "Sans Serif";
+      body.style.fontFamily = "Inter, sans serif";
+      inputText.style.fontFamily = "Inter, sans serif";
+      dropdownContent.classList.add("hide");
+    } else if (fontType === "Serif") {
+      fontText.textContent = "Serif";
+      inputText.style.fontFamily = "Lora, serif";
+      body.style.fontFamily = "Lora, serif";
+    } else if (fontType === "Mono") {
+      fontText.textContent = "Mono";
+      inputText.style.fontFamily = "Inconsolata, monospace";
+      body.style.fontFamily = "Inconsolata, monospace";
+    }
+
+    dropdownContent.classList.add("hide");
+  });
+});
+
+btnArrow.addEventListener("click", function () {
+  dropdownContent.classList.toggle("hide");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchData(inputText.value);
+  inputText.value = "";
+});
 
 // Add a click event listener to the theme toggle button
 themeToggle.addEventListener("click", () => {
   toggleTheme();
   updateThemeIcon();
 });
+
+// playButton.addEventListener("click", function() {
+
+// })
 
 // Check the user's preferred theme and set it if available
 const userPreferredTheme = localStorage.getItem("theme");
